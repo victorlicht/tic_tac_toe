79c54b3708530b8826d62b96865cd2849e174726
victorlicht <mihoubiabderrahmane09@gmail.com>
2023-08-05 13:04:10 +0100

    Add inputPlayerName(), playerInformation().

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
-struct player {
+typedef struct player {
     char name[32];
-    int score;
-};
-int main() {
+    int *score;
+}Player;
 
-}
 
 int checkWinner(char gameBoard[3][3]) {
     char players[2] = {'X', 'O'};
@@ -17,24 +16,44 @@ int checkWinner(char gameBoard[3][3]) {
             // Check the rows & columns
             if ((gameBoard[0][i] == currentPlayer && gameBoard[1][i] == currentPlayer && gameBoard[2][i] == currentPlayer)
              || (gameBoard[i][0] == currentPlayer && gameBoard[i][1] == currentPlayer && gameBoard[i][2] == currentPlayer))
+                // 1 means the player of 'X' who wins 2 is 'Y'
                 return (currentPlayer == 'X') ? 1 : 2;
             // Check diagonals
             if ((gameBoard[0][0] == currentPlayer && gameBoard[1][1] == currentPlayer && gameBoard[2][2] == currentPlayer)
              || (gameBoard[2][0] == currentPlayer && gameBoard[1][1] == currentPlayer && gameBoard[0][2] == currentPlayer))
+                // 1 means the player of 'X' who wins 2 is 'Y'
                 return (currentPlayer == 'X') ? 1 : 2;
         }
     }
+    // No Winner
     return 0;
 }
 
 void inputGame() {
-    int rowInput[1];
-    int columnInput[1];
-    int num[2];
+    int rowInput, columnInput;
     printf("Enter the row number: ");
-    fgets(rowInput, sizeof(rowInput), stdin);
-
+    if(scanf("%d", &rowInput) != 1 || (rowInput != 0 && rowInput != 1 && rowInput != 2)){
+        printf("Invalid Input!!");
+    }
+    printf("Enter the Column number: ");
+    if(scanf("%d", &columnInput) != 1 || (columnInput != 0 && columnInput != 1 && columnInput != 2)){
+        printf("Invalid Input!!");
     }
 }
+void inputPlayerName(Player *player, int playerID) {
+    // Function to get the name of players
+    printf("Player %d:\nEnter Your Name=: ", playerID);
+    scanf("%s", player->name);
+}
+void playerInformation(Player *player, int playerID) {
+    // Function to display the information of the two players.
+    printf("Player %d:\n Name=: %s\n", playerID, player->name);
+    printf("Score=: %d", *player->score);
+}
+
+int main() {
+    return 0;
+}
+
 
 
